9edd31eb2420323f5aba1d529fd2a4d91f14bee3
Ericpv91 <ericpvallejo@gmail.com>
2025-03-03 11:52:01 +0100

    app heroes

---
+++ src/heroes/pages/MarvelPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { HeroList } from '../components'
+
+export const MarvelPage = () => {
+  
+  return (
+    <>
+      <h1>Mavel Comics</h1>
+      <hr />
+
+      <HeroList publisher='Marvel Comics' />
+    </>
+  )
+}
